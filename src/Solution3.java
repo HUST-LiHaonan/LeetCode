26d148ff53e823eb7ea619db923d0fece7951f31
SpikeLi <spikeli1998@gmail.com>
2018-10-13 21:02:21 +0800

    第六题

---
+++ src/Solution3.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C), 2016-2018, 李浩楠
+ * FileName: Solution3
+ * Author:   mac
+ * Date:     2018/10/13 9:00 PM
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 〈zigzag-conversion〉<br>
+ * 〈〉
+ *
+ * @author mac
+ * @create 2018/10/13
+ * @since 1.0.0
+ */
+public class Solution3 {
+    public static String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+}
+
