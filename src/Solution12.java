8c231948f880f4d23b00a4fd6ea0ac1adae5411e
SpikeLi <spikeli1998@gmail.com>
2018-10-18 21:56:33 +0800

    十五题

--- src/Solution12.java
+++ src/Solution12.java
@@ -57,7 +57,7 @@ public class Solution12 {
     }
 
     public static void main(String[] args){
-        int[] nums ={};
+        int[] nums ={-1, 0, 1, 2, -1, -4};
         System.out.println(threeSum(nums));
     }
 }
