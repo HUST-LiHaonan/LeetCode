cc27d0ac73d2b8f76b1d36d45ae4ad90631c6632
SpikeLi <spikeli1998@gmail.com>
2018-11-06 09:44:04 +0800

    二十八题

---
+++ src/Solution24.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2016-2018, 李浩楠
+ * FileName: Solution24
+ * Author:   mac
+ * Date:     2018/11/5 8:58 PM
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈implement-strstr〉<对应第28题>
+ * 〈〉
+ *
+ * @author mac
+ * @create 2018/11/5
+ * @since 1.0.0
+ */
+public class Solution24 {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
+
