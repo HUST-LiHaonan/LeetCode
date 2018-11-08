bbc0d5a47ac7302917bde264fa2978cc8800e634
SpikeLi <spikeli1998@gmail.com>
2018-11-08 20:50:20 +0800

    三十一题

---
+++ src/Solution25.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C), 2016-2018, 李浩楠
+ * FileName: Solution25
+ * Author:   mac
+ * Date:     2018/11/8 7:41 PM
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈next-permutation〉<对应第31题>
+ * 〈〉
+ *
+ * @author mac
+ * @create 2018/11/8
+ * @since 1.0.0
+ */
+public class Solution25 {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
